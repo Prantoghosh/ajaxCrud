6abe7633633ee8a9ad0d17c34f8c01915a853757
Pranto Ghosh <p.pranto88@gmail.com>
2020-10-17 18:49:02 +0600

    error message shown 2 ways(list and individual field) and 422 eror handle

--- public/js/script.js
+++ public/js/script.js
@@ -14,42 +14,133 @@ $(document).ready(function () {
         var form = $(this);
         var url  = form.attr("action");
         var type = form.attr("method");
+
+        /*
+        This Below lines are to prevent 422 ajax+laravel error. If anything is defined 
+        required in controller(for example here "name" "email" "phone" is defined required in controller and "image" is
+        not defined as required in controller) 
+        it has to be handle in script.js like this. Otherwise it will give us an 422 error. so we handled name,email,phone here
         
+        */
 
-        $.ajax({
+        var name=$("#name").val();
+        var email=$("#email").val();
+        var phone=$("#phone").val();
+        var image=$("#image").val();
 
-            url:url,
-            data: new FormData(this),
-            type:type,
-            dataType:"JSON",
-            contentType:false,
-            cache:false,
-            processData:false,
-            enctype: 'multipart/form-data',
+        //console.log(name);
 
-            beforeSend:function(){
-                $(".load").fadeIn();
-            },
-            success:function(){
-                swal("Submitted","Form submitted successfully","success");
-                $('#addEmployee').trigger("reset");
-                $('#exampleModal').modal("hide");
-                
-                return getEmployeeData();
+        if(name==""){
+            swal("Required","Name field can't be empty","error");
+        }
 
-            },
-            complete:function(){
-                $(".load").fadeOut();
-            },
-
-            error:function(){
-                swal("Opps!","Form not submitted","error");
-                
-            }
+        else if(email==""){
+            swal("Required","Email field can't be empty","error");
+        }
 
 
+        else if(phone==""){
+            swal("Required","Phone field can't be empty","error");
+        }
 
-        });
+      else{
+
+                $.ajax({
+
+                    url:url,
+                    data: new FormData(this),
+                    type:type,
+                    dataType:"JSON",
+                    contentType:false,
+                    cache:false,
+                    processData:false,
+                    enctype: 'multipart/form-data',
+
+                    beforeSend:function(){
+                        $(".load").fadeIn();
+                    },
+                    success:function(){
+                        swal("Submitted","Form submitted successfully","success");
+                        $('#addEmployee').trigger("reset");
+                        $('#exampleModal').modal("hide");
+                        
+                        return getEmployeeData();
+
+                    },
+                    complete:function(){
+                        $(".load").fadeOut();
+                    },
+
+                    error:function(e){
+
+                    $(".load").fadeOut();
+
+                        
+
+                        //below block is for printing the erros like list
+                        
+                    //     $('#validation-errors').html('');
+                    //     $.each(e.responseJSON.errors, function(key,value) {
+                    //       $('#validation-errors').append('<div class="alert alert-danger">'+value+'</div');
+                    //       console.log(value);
+                    //   }); 
+
+
+                    //below block is for printing the erros like individual span -starts
+
+                    
+
+                    if(e.responseJSON.errors.name){
+                        $('#error_name').html('<span style="color:red">'+ e.responseJSON.errors.name[0] + '</span>');
+                    } 
+
+                    if(!e.responseJSON.errors.name){
+                        $('#error_name').html(' ');
+                    } 
+
+
+                    if(e.responseJSON.errors.email){
+                        $('#error_email').html('<span style="color:red">'+ e.responseJSON.errors.email[0] + '</span>');
+                    } 
+
+                    if(!e.responseJSON.errors.email){
+                        $('#error_email').html(' ');
+                    } 
+
+
+                    if(e.responseJSON.errors.phone){
+                        $('#error_phone').html('<span style="color:red">'+ e.responseJSON.errors.phone[0] + '</span>');
+                    } 
+
+                    if(!e.responseJSON.errors.phone){
+                        $('#error_phone').html(' ');
+                    }
+                    
+                    
+                    if(e.responseJSON.errors.image){
+                        $('#error_image').html('<span style="color:red">'+ e.responseJSON.errors.image[0] + '</span>');
+                    } 
+
+                    if(!e.responseJSON.errors.image){
+                        $('#error_image').html(' ');
+                    } 
+                    
+                    //below block is for printing the erros like individual span -Ends
+                    
+                    //$('#error_image').html('<span style="color:red">'+ e.responseJSON.errors.image[0] + '</span>');
+
+
+                        swal("Opps!","Form not submitted","error");
+                        console.log((e.responseJSON.errors));
+                        
+                    }
+
+
+
+                });
+
+        }
+
 
 
 
